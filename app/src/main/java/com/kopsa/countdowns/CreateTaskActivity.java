e2b0747c342ca6f5401932cc68d443c6160ccbcd
kkopsa <kopsak2@gmail.com>
2016-06-23 14:50:56 -0600

    revamp create task activity layout

--- app/src/main/java/com/kopsa/countdowns/CreateTaskActivity.java
+++ app/src/main/java/com/kopsa/countdowns/CreateTaskActivity.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.format.DateFormat;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -23,7 +25,7 @@ public class CreateTaskActivity extends AppCompatActivity {
 
     private static final String TAG = CreateTaskActivity.class.getSimpleName();
 
-    private Calendar mTaskDate;
+    private static Calendar mTaskDate;
     private String mTaskDesc;
 
     @Override
@@ -33,32 +35,79 @@ public class CreateTaskActivity extends AppCompatActivity {
 
         mTaskDate = Calendar.getInstance();
 
-        final DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
-        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
-        final Button saveButton = (Button) findViewById(R.id.add_task);
+        final ImageButton saveButton = (ImageButton) findViewById(R.id.add_task);
 
         assert saveButton != null;
-        assert timePicker != null;
-        assert datePicker != null;
-
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // set datetime to user's choice
-                mTaskDate.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth(),
-                        timePicker.getCurrentHour(), timePicker.getCurrentMinute());
-
                 // set task description
                 EditText et = (EditText) findViewById(R.id.task_desc);
+                assert et != null;
                 mTaskDesc = et.getText().toString();
 
                 Task task = new Task(mTaskDesc, mTaskDate);
-                TaskListSingleton.getInstance().addTask(task);
+                TaskListSingleton.getInstance();
+                TaskListSingleton.addTask(task);
 
                 setResult(Activity.RESULT_OK);
                 finish();
             }
         });
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View view) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            mTaskDate.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            mTaskDate.set(Calendar.MINUTE, minute);
+        }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            mTaskDate.set(year, month, day);
+        }
     }
 }
 
